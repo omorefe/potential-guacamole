e1c7edf88c7cdfad6389e8acd707b30131f9dfad
Awesome Dude <72664885+OfficialAwesomeDude@users.noreply.github.com>
2021-03-09 10:03:49 -0800

    Add files via upload

--- click_game/popup/script.js
+++ click_game/popup/script.js
@@ -1,21 +1,3 @@
-// setTimeout(() => {
-//         let button = document.createElement('button')
-//         button.style = `
-//             position: absolute;
-//             top: ${Math.floor(Math.random()*500)}px;
-//             left: ${Math.floor(Math.random()*500)}px;
-//             width: 30px;
-//             height: 30px;
-//         `
-//         button.addEventListener('click', () => {
-//             button.style.top = `${Math.floor(Math.random()*300)}px`
-//             button.style.left = `${Math.floor(Math.random()*300)}px`
-//         })
-//         document.body.appendChild(button)
-// }, 1000)
-
-// const newButton = e => e.currentTarget.replaceWith(createButton());
-
 let elm = document.createElement("button")
 elm.style = `
     position: absolute;
@@ -30,25 +12,3 @@ elm.addEventListener('click', () => {
 })
 
 document.body.append(elm);
-
-
-
-// let setAttributes = e => {
-//     e.style.position = 'absolute';
-//     e.style.top = Math.floor(Math.random()*500) + 'px';
-//     e.style.left = Math.floor(Math.random()*500) + 'px';
-//     e.style.width = '10px';
-//     e.style.height = '10px';
-//     e.addEventListener('click', () => {newButton()})
-// }
-// let newButton = () => {
-//     document.body.removeChild(document.body.querySelector('button'))
-//     let e = document.createElement('button');
-//     document.body.appendChild(e);
-//     setAttributes(e)
-// }
-// setTimeout(() => {
-//     let e = document.createElement('button');
-//     setAttributes(e)
-//     document.body.appendChild(e)
-// }, 1000);
