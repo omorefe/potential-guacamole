d6798a3459f110eadb9ccb60dd20410b7dde2bb1
Awesome Dude <72664885+OfficialAwesomeDude@users.noreply.github.com>
2021-03-10 09:26:45 -0800

    Update script.js

--- click_game/popup/script.js
+++ click_game/popup/script.js
@@ -4,17 +4,17 @@ let buttons = {
 
 for (let i = 0; i < buttons.amount; i++) {
     let elm = document.createElement("button")
-let score = document.getElementById("score-constant")
+    let score = document.getElementById("score-constant")
 
-elm.classList.add('button')
-elm.style.top = `${Math.floor(Math.random()*270) + 15}px`
-elm.style.left = `${Math.floor(Math.random()*270) + 15}px`
-
-elm.addEventListener('click', () => {
-    score.innerHTML = parseInt(score.innerHTML) + 1
+    elm.classList.add('button')
     elm.style.top = `${Math.floor(Math.random()*270) + 15}px`
     elm.style.left = `${Math.floor(Math.random()*270) + 15}px`
-})
 
-document.body.append(elm);
+    elm.addEventListener('click', () => {
+        score.innerHTML = parseInt(score.innerHTML) + 1
+        elm.style.top = `${Math.floor(Math.random()*270) + 15}px`
+        elm.style.left = `${Math.floor(Math.random()*270) + 15}px`
+    })
+
+    document.body.append(elm);
 }
