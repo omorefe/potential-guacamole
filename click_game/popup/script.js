2f30ddd3bee05a3eb9ffdf05e38d0157e49982dc
AwesomeDude <72664885+OfficialAwesomeDude@users.noreply.github.com>
2021-03-12 19:18:32 -0800

    Update
    hmm yes

--- click_game/popup/script.js
+++ click_game/popup/script.js
@@ -1,20 +1,32 @@
 let buttons = {
     amount: 3
 }
+let score = document.getElementById("score-constant")
 
-for (let i = 0; i < buttons.amount; i++) {
-    let elm = document.createElement("button")
-    let score = document.getElementById("score-constant")
-
-    elm.classList.add('button')
-    elm.style.top = `${Math.floor(Math.random()*270) + 15}px`
-    elm.style.left = `${Math.floor(Math.random()*270) + 15}px`
-
-    elm.addEventListener('click', () => {
-        score.innerHTML = parseInt(score.innerHTML) + 1
-        elm.style.top = `${Math.floor(Math.random()*270) + 15}px`
-        elm.style.left = `${Math.floor(Math.random()*270) + 15}px`
-    })
-
-    document.body.append(elm);
+let reset = () => {
+    let btns = document.querySelectorAll('.button')
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].remove();
+    }
+    score.innerHTML = '0'
+    startSandbox()
 }
+
+let startSandbox = () => {
+    for (let i = 0; i < buttons.amount; i++) {
+        let elm = document.createElement("button")
+
+        elm.classList.add('button')
+        elm.style.top = `${Math.floor(Math.random()*270)}px`
+        elm.style.left = `${Math.floor(Math.random()*270)}px`
+
+        elm.addEventListener('click', () => {
+            score.innerHTML = parseInt(score.innerHTML) + 1
+            elm.style.top = `${Math.floor(Math.random()*270)}px`
+            elm.style.left = `${Math.floor(Math.random()*270)}px`
+        })
+
+        document.body.append(elm);
+    }
+}
+startSandbox()
